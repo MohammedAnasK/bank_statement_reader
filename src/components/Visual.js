44c2dd2f6cb1f6be086363310ddc9776c62d21ae
jenish <panditjenish5@gmail.com>
2024-03-05 19:50:48 +0530

    create Qna and services

---
+++ src/components/Visual.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Visual = () => {
+  return (
+    <div className=' h-full'>
+      <img
+        src='bank_statement_reader\event-image1.jpg'
+        alt="df"
+      />
+    </div>
+  );
+};
+
+export default Visual;
